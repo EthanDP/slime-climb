dbef87ae828fbc060224fb81ffd494741d6216e5
Ethan <38187410+EthanDP@users.noreply.github.com>
2018-04-09 19:39:58 -0500

    Lava + minor changes
    I still have to fix the camera so it doesn't attempt to follow the player even after it dies.

---
+++ GT Project/Assets/Scripts/LavaController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaController : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update () {
+		transform.Translate (0, .3f * Time.deltaTime, 0);	
+	}
+
+	void OnTriggerEnter2D (Collider2D coll) {
+		Destroy (player);
+	}
+}
