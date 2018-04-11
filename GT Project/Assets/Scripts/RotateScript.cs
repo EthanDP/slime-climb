b3b4a03ed45563982f94b1e878791d284f08bfdb
Shaan Hyder <Generalflup@gmail.com>
2018-04-11 11:35:50 -0500

    fix quick

--- GT Project/Assets/Scripts/RotateScript.cs
+++ GT Project/Assets/Scripts/RotateScript.cs
@@ -16,22 +16,31 @@ public class RotateScript : MonoBehaviour {
 
 	void OnCollisionEnter2D (Collision2D coll) {
 
-		RaycastHit2D hit;
-		//Spits a raycast left, right and down, rotates to be perpendicular the ray hits the object colliding with
-		hit = Physics2D.Raycast (transform.position, Vector2.down);
-		if (hit.collider.gameObject.Equals (coll.gameObject) && !hasRotated) {
-			transform.rotation = Quaternion.Euler (Vector3.zero);
-			hasRotated = true;
-		}
-		hit = Physics2D.Raycast (transform.position, Vector2.left);
-		if (hit.collider.gameObject.Equals (coll.gameObject) && !hasRotated) {
-			transform.rotation = Quaternion.Euler (new Vector3 (0, 0, -90));
-			hasRotated = true;
-		}
-		hit = Physics2D.Raycast (transform.position, Vector2.right);
-		if (hit.collider.gameObject.Equals (coll.gameObject) && !hasRotated) {
-			transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 90));
-		}
+        try
+        {
+            RaycastHit2D hit;
+            //Spits a raycast left, right and down, rotates to be perpendicular the ray hits the object colliding with
+            hit = Physics2D.Raycast(transform.position, Vector2.down);
+            if (hit.collider.gameObject.Equals(coll.gameObject) && !hasRotated)
+            {
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+                hasRotated = true;
+            }
+            hit = Physics2D.Raycast(transform.position, Vector2.left);
+            if (hit.collider.gameObject.Equals(coll.gameObject) && !hasRotated)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+                hasRotated = true;
+            }
+            hit = Physics2D.Raycast(transform.position, Vector2.right);
+            if (hit.collider.gameObject.Equals(coll.gameObject) && !hasRotated)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            }
+        } catch
+        {
+
+        }
 
 	}
 
