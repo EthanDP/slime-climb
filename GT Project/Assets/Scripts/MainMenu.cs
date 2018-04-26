71a5bbff8b02a95e6c7b06cbc21d42fd2570f199
Ethan <38187410+EthanDP@users.noreply.github.com>
2018-04-25 19:25:18 -0500

    Revert "oh"
    This reverts commit cc1e89163657ae26f7f759fa950fbcf5dde6c9c9.

--- GT Project/Assets/Scripts/MainMenu.cs
+++ GT Project/Assets/Scripts/MainMenu.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
 
-public class MenuController : MonoBehaviour {
-
-	public int levelNumber;
+public class MainMenu : MonoBehaviour {
 
 	public void LevelSelect () {
 		EditorSceneManager.LoadScene (EditorSceneManager.GetActiveScene ().buildIndex + 1);
@@ -18,9 +16,5 @@ public class MenuController : MonoBehaviour {
 	public void Quit () {
 		Application.Quit ();
 
-	}
-
-	public void LevelChoice () {
-		EditorSceneManager.LoadScene (EditorSceneManager.GetActiveScene().buildIndex + levelNumber);
 	}
 }
