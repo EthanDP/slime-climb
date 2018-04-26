cc1e89163657ae26f7f759fa950fbcf5dde6c9c9
Ethan <38187410+EthanDP@users.noreply.github.com>
2018-04-25 19:19:44 -0500

    oh

--- GT Project/Assets/Scripts/MenuController.cs
+++ GT Project/Assets/Scripts/MenuController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MenuController : MonoBehaviour {
+
+	public int levelNumber;
 
 	public void LevelSelect () {
 		EditorSceneManager.LoadScene (EditorSceneManager.GetActiveScene ().buildIndex + 1);
@@ -16,5 +18,9 @@ public class MainMenu : MonoBehaviour {
 	public void Quit () {
 		Application.Quit ();
 
+	}
+
+	public void LevelChoice () {
+		EditorSceneManager.LoadScene (EditorSceneManager.GetActiveScene().buildIndex + levelNumber);
 	}
 }
