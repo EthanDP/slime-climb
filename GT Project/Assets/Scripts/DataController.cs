f442fd81d595fa79afb2e46bc239b2f878ca8b15
Ethan <38187410+EthanDP@users.noreply.github.com>
2018-04-26 18:35:19 -0500

    yeee yee papi

---
+++ GT Project/Assets/Scripts/DataController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class DataController : MonoBehaviour {
+
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Update () {
+		
+	}
+}
