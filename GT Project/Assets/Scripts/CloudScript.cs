5dee7dc12ec137efafa19e5cff78a0d3c9d2cf4b
Ethan <38187410+EthanDP@users.noreply.github.com>
2018-04-27 21:16:00 -0500

    Added cloud, did some level stuff
    we need to fix the little bouncy evil enemy boy.  He sometimes gets stuck and wont jump if he lands on the edge of a platform or perfectly in a corner

---
+++ GT Project/Assets/Scripts/CloudScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (player.transform.position.y - .055f >= transform.position.y) {
+			gameObject.GetComponent<BoxCollider2D> ().isTrigger = false;
+		} else {
+			gameObject.GetComponent<BoxCollider2D> ().isTrigger = true;
+		}
+	}
+}
