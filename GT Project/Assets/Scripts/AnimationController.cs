7d9074d58822e74c049e90fd27bf94d05da34f1a
Ethan <38187410+EthanDP@users.noreply.github.com>
2018-05-09 17:28:01 -0500

    Finished Level Animations
    That was a big ol pain

---
+++ GT Project/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	public void PlayAnimation(string animation) {
+		GetComponent<Animator> ().Play (animation);
+	}
+}
