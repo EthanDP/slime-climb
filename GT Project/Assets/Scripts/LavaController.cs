f59073edf7253bc7f577f57354bcc541a28b8c4b
Ethan <38187410+EthanDP@users.noreply.github.com>
2018-05-11 22:31:54 -0500

    Thumbnails, Lava, and Level 4

--- GT Project/Assets/Scripts/LavaController.cs
+++ GT Project/Assets/Scripts/LavaController.cs
@@ -6,18 +6,24 @@ public class LavaController : MonoBehaviour {
 
 	GameObject deathHandler;
 
+	bool isMoving = true;
 
 	void Start () {
 		deathHandler = GameObject.FindGameObjectWithTag ("DeathEvent");
 	}
 
 	void Update () {
-		transform.Translate (0, .3f * Time.deltaTime, 0);	
+		if (isMoving) {
+			transform.Translate (0, .3f * Time.deltaTime, 0);	
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D coll) {
 		if (coll.tag == "Player") {
 			deathHandler.GetComponent<DeathHandler> ().PlayerDeath ();
+			isMoving = false;
+		} else if (coll.tag == "Danger") {
+			Destroy (coll.gameObject);
 		}
 	}
 }
